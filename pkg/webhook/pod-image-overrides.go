fa8e4329b538d90c6cb33d98d301b4cb9d347645
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2022-03-04 21:07:17 +0000

    Fix overriding pod images

--- pkg/webhook/pod-image-overrides.go
+++ pkg/webhook/pod-image-overrides.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/martian/log"
 	admissionv1 "k8s.io/api/admission/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -16,10 +15,11 @@ import (
 func (s *Server) overridePodImages(c *gin.Context) {
 	request := admissionv1.AdmissionReview{}
 	response := admissionv1.AdmissionReview{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "admission.k8s.io/v1",
+			Kind:       "AdmissionReview",
+		},
 		Response: &admissionv1.AdmissionResponse{},
-	}
-	if request.Request != nil {
-		response.Response.UID = request.Request.UID
 	}
 
 	body, err := ioutil.ReadAll(c.Request.Body)
@@ -27,17 +27,23 @@ func (s *Server) overridePodImages(c *gin.Context) {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
+
 	_, _, err = codecs.UniversalDeserializer().Decode(body, nil, &request)
 	if err != nil {
 		s.log.Error(err)
 		response.Response.Result = &metav1.Status{
 			Message: err.Error(),
 		}
-	} else if request.Request != nil && request.Request.Resource.Resource == "pods" {
+		c.JSON(http.StatusOK, response)
+		return
+	}
 
-		log.Debugf("Admission webhook checking for image overrides for pod %s/%s", request.Request.Namespace, request.Request.Name)
-		response.Response.Allowed = true
+	response.Response.Allowed = true
+	if request.Request != nil {
+		response.Response.UID = request.Request.UID
+	}
 
+	if request.Request != nil && request.Request.Resource.Resource == "pods" {
 		raw := request.Request.Object.Raw
 		pod := corev1.Pod{}
 		deserializer := codecs.UniversalDeserializer()
@@ -49,6 +55,8 @@ func (s *Server) overridePodImages(c *gin.Context) {
 			return
 		}
 
+		s.log.Debugf("Admission webhook checking for image overrides for pod %s/%s", pod.Namespace, pod.Name)
+
 		var patches []string
 		for i, container := range pod.Spec.InitContainers {
 			newImage, ok := s.podImageOverrides[container.Image]
@@ -57,7 +65,7 @@ func (s *Server) overridePodImages(c *gin.Context) {
 			}
 			patch := fmt.Sprintf(`{"op":"replace","path":"/spec/initContainers/%d/image","value":"%s"}`, i, newImage)
 			patches = append(patches, patch)
-			log.Infof("Overriding image %q with %q in pod %s/%s", container.Image, newImage, pod.Namespace, pod.Name)
+			s.log.Infof("Overriding image %q with %q in pod %s/%s", container.Image, newImage, pod.Namespace, pod.Name)
 		}
 
 		for i, container := range pod.Spec.Containers {
@@ -67,11 +75,11 @@ func (s *Server) overridePodImages(c *gin.Context) {
 			}
 			patch := fmt.Sprintf(`{"op":"replace","path":"/spec/containers/%d/image","value":"%s"}`, i, newImage)
 			patches = append(patches, patch)
-			log.Infof("Overriding image %q with %q in pod %s/%s", container.Image, newImage, pod.Namespace, pod.Name)
+			s.log.Infof("Overriding image %q with %q in pod %s/%s", container.Image, newImage, pod.Namespace, pod.Name)
 		}
 
 		if len(patches) == 0 {
-			log.Debugf("Admission webhook found no image overrides for pod %s/%s", request.Request.Namespace, request.Request.Name)
+			s.log.Debugf("Admission webhook found no image overrides for pod %s/%s", pod.Namespace, pod.Name)
 		} else {
 			pt := admissionv1.PatchTypeJSONPatch
 			response.Response.PatchType = &pt
@@ -79,8 +87,7 @@ func (s *Server) overridePodImages(c *gin.Context) {
 			response.Response.Patch = []byte(patch)
 		}
 	} else {
-		log.Debugf("Admission webhook ignoring %s/%s", request.Request.Namespace, request.Request.Name)
-		response.Response.Allowed = true
+		s.log.Debugf("Admission webhook ignoring %s/%s", request.Request.Namespace, request.Request.Name)
 	}
 
 	c.JSON(http.StatusOK, response)
