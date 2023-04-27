1eb36daf8fca31345c5bd9c579032f0f2e751f9c
Andrew Lavery <laverya@umich.edu>
2023-04-26 22:28:47 -0400

    refactor out object store code

---
+++ pkg/objectstore/migrate.go
@@ -0,0 +1,179 @@
+package objectstore
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/replicatedhq/ekco/pkg/util"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+// SyncAllBuckets syncs copies all objects in all buckets in object store to another, and returns progress via a channel.
+func SyncAllBuckets(ctx context.Context, sourceEndpoint, sourceAccessKey, sourceSecretKey, destEndpoint, destAccessKey, destSecretKey string, logs chan<- string) error {
+	// Initialize source client object.
+	minioClient, err := minio.New(sourceEndpoint, &minio.Options{
+		Creds: credentials.NewStaticV4(sourceAccessKey, sourceSecretKey, ""),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to initialize minio client: %v", err)
+	}
+
+	// Initialize dest client object.
+	rookClient, err := minio.New(destEndpoint, &minio.Options{
+		Creds: credentials.NewStaticV4(destAccessKey, destSecretKey, ""),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to initialize rook client: %v", err)
+	}
+
+	if logs != nil {
+		logs <- "Initialized clients"
+	}
+
+	minioBuckets, err := minioClient.ListBuckets(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to list minio buckets: %v", err)
+	}
+
+	if logs != nil {
+		logs <- fmt.Sprintf("Found %d buckets to sync", len(minioBuckets))
+	}
+
+	for _, bucket := range minioBuckets {
+		numObjects, err := syncBucket(ctx, minioClient, rookClient, bucket.Name)
+		if err != nil {
+			return fmt.Errorf("failed to sync bucket %s: %v", bucket.Name, err)
+		}
+
+		if logs != nil {
+			logs <- fmt.Sprintf("Copied %d objects in %s", numObjects, bucket.Name)
+		}
+	}
+
+	return nil
+}
+
+// UpdateConsumers updates the access key and secret key for all consumers of the object store.
+// it handles kotsadm, registry, and velero.
+func UpdateConsumers(ctx context.Context, client kubernetes.Interface, endpoint, hostname, accessKey, secretKey string) error {
+	// if kubernetes_resource_exists default secret kotsadm-s3
+	kotsadmS3, err := client.CoreV1().Secrets("default").Get(ctx, "kotsadm-s3", metav1.GetOptions{})
+	if err != nil {
+		if !k8serrors.IsNotFound(err) {
+			return fmt.Errorf("get kotsadm-s3 secret in default namespace: %v", err)
+		}
+	}
+	if kotsadmS3 != nil {
+		kotsadmS3.Data["access-key-id"] = []byte(accessKey)
+		kotsadmS3.Data["secret-access-key"] = []byte(secretKey)
+		kotsadmS3.Data["endpoint"] = []byte(hostname)
+		kotsadmS3.Data["object-store-cluster-ip"] = []byte(endpoint)
+
+		_, err = client.CoreV1().Secrets("default").Update(ctx, kotsadmS3, metav1.UpdateOptions{})
+		if err != nil {
+			return fmt.Errorf("update kotsadm-s3 secret in default namespace: %v", err)
+		}
+
+		err = util.RestartDeployment(ctx, client, "default", "kotsadm")
+		if err != nil {
+			return fmt.Errorf("restart kotsadm deployment after migrating object store: %v", err)
+		}
+		err = util.RestartStatefulset(ctx, client, "default", "kotsadm")
+		if err != nil {
+			return fmt.Errorf("restart kotsadm statefulset after migrating object store: %v", err)
+		}
+	}
+
+	// if the 'registry-config' configmap and the 'registry-s3-secret' secret exists in the kurl namespace
+	registryConfig, err := client.CoreV1().ConfigMaps("kurl").Get(ctx, "registry-config", metav1.GetOptions{})
+	if err != nil {
+		if !k8serrors.IsNotFound(err) {
+			return fmt.Errorf("get registry-config configmap in kurl namespace: %v", err)
+		}
+	}
+	registrySecret, err := client.CoreV1().Secrets("kurl").Get(ctx, "registry-s3-secret", metav1.GetOptions{})
+	if err != nil {
+		if !k8serrors.IsNotFound(err) {
+			return fmt.Errorf("get registry-s3-secret secret in kurl namespace: %v", err)
+		}
+	}
+	if registryConfig != nil && registrySecret != nil {
+		existingConfig := registryConfig.Data["config.yml"]
+		newConfig := regexp.MustCompile(`regionendpoint: http.*`).ReplaceAllString(existingConfig, fmt.Sprintf("regionendpoint: http://%s/", endpoint))
+		registryConfig.Data["config.yml"] = newConfig
+
+		_, err = client.CoreV1().ConfigMaps("kurl").Update(ctx, registryConfig, metav1.UpdateOptions{})
+		if err != nil {
+			return fmt.Errorf("update registry-config configmap in kurl namespace: %v", err)
+		}
+
+		if registrySecret.StringData == nil {
+			registrySecret.StringData = map[string]string{}
+		}
+
+		registrySecret.StringData["access-key-id"] = accessKey
+		registrySecret.StringData["secret-access-key"] = secretKey
+		registrySecret.StringData["object-store-cluster-ip"] = endpoint
+		registrySecret.StringData["object-store-hostname"] = hostname
+
+		_, err = client.CoreV1().Secrets("kurl").Update(ctx, registrySecret, metav1.UpdateOptions{})
+		if err != nil {
+			return fmt.Errorf("update registry-s3-secret secret in kurl namespace: %v", err)
+		}
+
+		err = util.RestartDeployment(ctx, client, "kurl", "registry")
+		if err != nil {
+			return fmt.Errorf("restart registry deployment after migrating object store: %v", err)
+		}
+	}
+
+	//TODO if kubernetes_resource_exists velero backupstoragelocation default
+
+	//TODO if kubernetes_resource_exists velero secret cloud-credentials
+
+	return nil
+}
+
+func syncBucket(ctx context.Context, src *minio.Client, dst *minio.Client, bucket string) (int, error) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	count := 0
+
+	exists, err := dst.BucketExists(ctx, bucket)
+	if err != nil {
+		return count, fmt.Errorf("failed to check if bucket %q exists in destination: %v", bucket, err)
+	}
+	if !exists {
+		if err := dst.MakeBucket(ctx, bucket, minio.MakeBucketOptions{}); err != nil {
+			return count, fmt.Errorf("failed to make bucket %q in destination: %v", bucket, err)
+		}
+	}
+
+	srcObjectInfoChan := src.ListObjects(ctx, bucket, minio.ListObjectsOptions{Recursive: true})
+
+	for srcObjectInfo := range srcObjectInfoChan {
+		srcObject, err := src.GetObject(ctx, bucket, srcObjectInfo.Key, minio.GetObjectOptions{})
+		if err != nil {
+			return count, fmt.Errorf("get %s from source: %v", srcObjectInfo.Key, err)
+		}
+
+		_, err = dst.PutObject(ctx, bucket, srcObjectInfo.Key, srcObject, srcObjectInfo.Size, minio.PutObjectOptions{
+			ContentType:     srcObjectInfo.ContentType,
+			ContentEncoding: srcObjectInfo.Metadata.Get("Content-Encoding"),
+		})
+		_ = srcObject.Close()
+		if err != nil {
+			return count, fmt.Errorf("failed to copy object %s to destination: %v", srcObjectInfo.Key, err)
+		}
+
+		count++
+	}
+
+	return count, nil
+}
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -5,13 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"regexp"
 	"sync"
 
-	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/replicatedhq/ekco/pkg/ekcoops"
-	"github.com/replicatedhq/ekco/pkg/util"
+	"github.com/replicatedhq/ekco/pkg/objectstore"
 	"github.com/replicatedhq/pvmigrate/pkg/migrate"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -39,7 +36,7 @@ func Serve(config ekcoops.Config, client kubernetes.Interface) {
 		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte(migrationStatus))
 		if err != nil {
-			log.Printf("failed to write status: %v", err)
+			log.Printf("write status: %v", err)
 		}
 	})
 
@@ -47,7 +44,7 @@ func Serve(config ekcoops.Config, client kubernetes.Interface) {
 		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte(migrationLogs))
 		if err != nil {
-			log.Printf("failed to write logs: %v", err)
+			log.Printf("write logs: %v", err)
 		}
 	})
 
@@ -57,13 +54,13 @@ func Serve(config ekcoops.Config, client kubernetes.Interface) {
 		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte("APPROVED"))
 		if err != nil {
-			log.Printf("failed to write approval: %v", err)
+			log.Printf("write approval: %v", err)
 		}
 	})
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		log.Fatalf("failed to start server: %v", err)
+		log.Fatalf("start server: %v", err)
 	}
 }
 
@@ -74,7 +71,7 @@ func migrateStorage(config ekcoops.Config, client kubernetes.Interface) {
 		return
 	}
 
-	fmt.Printf("starting storage migration\n")
+	migrationLogs += "starting storage migration\n"
 
 	// TODO: pause the operator loop
 
@@ -82,58 +79,26 @@ func migrateStorage(config ekcoops.Config, client kubernetes.Interface) {
 	err := migrateObjectStorage(config, client)
 	if err != nil {
 		migrationStatus = MIGRATION_STATUS_FAILED
-		migrationLogs = fmt.Sprintf("failed to migrate object storage: %v", err)
+		migrationLogs += fmt.Sprintf("migrate object storage: %v", err)
 		return
 	}
 
-	fmt.Printf("starting pvmigrate\n")
+	migrationLogs += "starting pvmigrate\n"
 
 	migrationStatus = MIGRATION_STATUS_PVCMIGRATE
 	err = migrateStorageClasses(config, client)
 	if err != nil {
 		migrationStatus = MIGRATION_STATUS_FAILED
-		migrationLogs = fmt.Sprintf("failed to migrate storage classes: %v", err)
+		migrationLogs += fmt.Sprintf("migrate storage classes: %v", err)
 		return
 	}
 
-	fmt.Printf("storage migration completed\n")
+	migrationLogs += "storage migration completed\n"
 
 	migrationStatus = MIGRATION_STATUS_COMPLETED
 }
 
 // TODO move to 'minio' package, call from 'cluster' too
-func isMinioInUse(config ekcoops.Config, client kubernetes.Interface) (bool, error) {
-	// if the minio NS does not exist, it is not in use
-	_, err := client.CoreV1().Namespaces().Get(context.TODO(), config.MinioNamespace, metav1.GetOptions{})
-	if err != nil {
-		if k8serrors.IsNotFound(err) {
-			return false, nil
-		} else {
-			return false, fmt.Errorf("failed to get minio namespace %s: %v", config.MinioNamespace, err)
-		}
-	}
-
-	// if the minio deployment and statefulset do not exist, there is nothing to migrate
-	_, err = client.AppsV1().Deployments(config.MinioNamespace).Get(context.TODO(), "minio", metav1.GetOptions{})
-	if err == nil {
-		return true, nil
-	} else {
-		if !k8serrors.IsNotFound(err) {
-			return false, fmt.Errorf("failed to get minio deployment in %s: %v", config.MinioNamespace, err)
-		}
-	}
-
-	_, err = client.AppsV1().StatefulSets(config.MinioNamespace).Get(context.TODO(), "ha-minio", metav1.GetOptions{})
-	if err != nil {
-		if k8serrors.IsNotFound(err) {
-			return false, nil
-		} else {
-			return false, fmt.Errorf("failed to get minio statefulset in %s: %v", config.MinioNamespace, err)
-		}
-	}
-
-	return true, nil
-}
 
 // check if minio is in use
 // if so, disable the minio service to prevent race conditions with files being updated during the migration
@@ -142,9 +107,9 @@ func isMinioInUse(config ekcoops.Config, client kubernetes.Interface) (bool, err
 // finally, delete the minio namespace and contents
 // TODO factor out, call from cluster package too
 func migrateObjectStorage(config ekcoops.Config, client kubernetes.Interface) error {
-	minioInUse, err := isMinioInUse(config, client)
+	minioInUse, err := objectstore.IsMinioInUse(context.TODO(), client, config.MinioNamespace)
 	if err != nil {
-		return fmt.Errorf("failed to check if minio is in use: %v", err)
+		return fmt.Errorf("check if minio is in use: %v", err)
 	}
 	if !minioInUse {
 		return nil
@@ -192,173 +157,36 @@ func migrateObjectStorage(config ekcoops.Config, client kubernetes.Interface) er
 		return fmt.Errorf("disable existing minio service: %w", err)
 	}
 
-	// Initialize minio client object.
-	minioClient, err := minio.New(fmt.Sprintf("%s:9000", minioPodIP), &minio.Options{
-		Creds: credentials.NewStaticV4(minioAccessKey, minioSecretKey, ""),
-	})
+	// migrate all data from minio to rook
+	// TODO use progress writer
+	err = objectstore.SyncAllBuckets(context.TODO(), fmt.Sprintf("%s:9000", minioPodIP), minioAccessKey, minioSecretKey, rookEndpoint, rookAccessKey, rookSecretKey, nil)
 	if err != nil {
-		return fmt.Errorf("failed to initialize minio client: %v", err)
-	}
-
-	// Initialize rook client object.
-	rookClient, err := minio.New(rookEndpoint, &minio.Options{
-		Creds: credentials.NewStaticV4(rookAccessKey, rookSecretKey, ""),
-	})
-	if err != nil {
-		return fmt.Errorf("failed to initialize rook client: %v", err)
-	}
-
-	fmt.Printf("determining buckets to migrate\n")
-
-	minioBuckets, err := minioClient.ListBuckets(context.TODO())
-	if err != nil {
-		return fmt.Errorf("failed to list minio buckets: %v", err)
-	}
-
-	for _, bucket := range minioBuckets {
-		fmt.Printf("migrating bucket %s\n", bucket.Name)
-		numObjects, err := syncBucket(context.TODO(), minioClient, rookClient, bucket.Name)
-		if err != nil {
-			return fmt.Errorf("failed to sync bucket %s: %v", bucket.Name, err)
-		}
-		migrationLogs += fmt.Sprintf("synced %d objects in bucket %s\n", numObjects, bucket.Name)
+		return fmt.Errorf("migrate data from minio to rook: %w", err)
 	}
 
 	// update secrets in the cluster to point to the new rook object store
-	fmt.Printf("updating secrets in the cluster to point to the new rook object store\n")
-
-	// if kubernetes_resource_exists default secret kotsadm-s3
-	kotsadmS3, err := client.CoreV1().Secrets("default").Get(context.TODO(), "kotsadm-s3", metav1.GetOptions{})
+	migrationLogs += "updating secrets in the cluster to point to the new rook object store\n"
+	err = objectstore.UpdateConsumers(context.TODO(), client, rookEndpoint, "http://rook-ceph-rgw-rook-ceph-store.rook-ceph", rookAccessKey, rookSecretKey)
 	if err != nil {
-		if !k8serrors.IsNotFound(err) {
-			return fmt.Errorf("failed to get kotsadm-s3 secret in default namespace: %v", err)
-		}
-	}
-	if kotsadmS3 != nil {
-		kotsadmS3.Data["access-key-id"] = []byte(rookAccessKey)
-		kotsadmS3.Data["secret-access-key"] = []byte(rookSecretKey)
-		kotsadmS3.Data["endpoint"] = []byte("http://rook-ceph-rgw-rook-ceph-store.rook-ceph")
-		kotsadmS3.Data["object-store-cluster-ip"] = []byte(rookEndpoint)
-
-		_, err = client.CoreV1().Secrets("default").Update(context.TODO(), kotsadmS3, metav1.UpdateOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to update kotsadm-s3 secret in default namespace: %v", err)
-		}
-
-		err = util.RestartDeployment(context.TODO(), client, "default", "kotsadm")
-		if err != nil {
-			return fmt.Errorf("failed to restart kotsadm deployment after migrating object store: %v", err)
-		}
-		err = util.RestartStatefulset(context.TODO(), client, "default", "kotsadm")
-		if err != nil {
-			return fmt.Errorf("failed to restart kotsadm statefulset after migrating object store: %v", err)
-		}
+		return fmt.Errorf("update secrets in the cluster to point to the new rook object store: %w", err)
 	}
 
-	// if the 'registry-config' configmap and the 'registry-s3-secret' secret exists in the kurl namespace
-	registryConfig, err := client.CoreV1().ConfigMaps("kurl").Get(context.TODO(), "registry-config", metav1.GetOptions{})
-	if err != nil {
-		if !k8serrors.IsNotFound(err) {
-			return fmt.Errorf("failed to get registry-config configmap in kurl namespace: %v", err)
-		}
-	}
-	registrySecret, err := client.CoreV1().Secrets("kurl").Get(context.TODO(), "registry-s3-secret", metav1.GetOptions{})
-	if err != nil {
-		if !k8serrors.IsNotFound(err) {
-			return fmt.Errorf("failed to get registry-s3-secret secret in kurl namespace: %v", err)
-		}
-	}
-	if registryConfig != nil && registrySecret != nil {
-		existingConfig := registryConfig.Data["config.yml"]
-		newConfig := regexp.MustCompile(`regionendpoint: http.*`).ReplaceAllString(existingConfig, fmt.Sprintf("regionendpoint: http://%s/", "TODO ROOK OBJECT STORE CLUSTER IP"))
-		registryConfig.Data["config.yml"] = newConfig
-
-		_, err = client.CoreV1().ConfigMaps("kurl").Update(context.TODO(), registryConfig, metav1.UpdateOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to update registry-config configmap in kurl namespace: %v", err)
-		}
-
-		if registrySecret.StringData == nil {
-			registrySecret.StringData = map[string]string{}
-		}
-
-		registrySecret.StringData["access-key-id"] = rookAccessKey
-		registrySecret.StringData["secret-access-key"] = rookSecretKey
-		registrySecret.StringData["object-store-cluster-ip"] = rookEndpoint
-		registrySecret.StringData["object-store-hostname"] = "http://rook-ceph-rgw-rook-ceph-store.rook-ceph"
-
-		_, err = client.CoreV1().Secrets("kurl").Update(context.TODO(), registrySecret, metav1.UpdateOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to update registry-s3-secret secret in kurl namespace: %v", err)
-		}
-
-		err = util.RestartDeployment(context.TODO(), client, "kurl", "registry")
-		if err != nil {
-			return fmt.Errorf("failed to restart registry deployment after migrating object store: %v", err)
-		}
+	err = client.AppsV1().StatefulSets(config.MinioNamespace).Delete(context.TODO(), "ha-minio", metav1.DeleteOptions{})
+	if err != nil && !k8serrors.IsNotFound(err) {
+		return fmt.Errorf("delete minio statefulset: %v", err)
 	}
 
-	// if kubernetes_resource_exists velero backupstoragelocation default TODO
+	err = client.AppsV1().Deployments(config.MinioNamespace).Delete(context.TODO(), "minio", metav1.DeleteOptions{})
+	if err != nil && !k8serrors.IsNotFound(err) {
+		return fmt.Errorf("delete minio deployment: %v", err)
+	}
 
-	// if kubernetes_resource_exists velero secret cloud-credentials TODO
-
-	//err = client.AppsV1().StatefulSets(config.MinioNamespace).Delete(context.TODO(), "ha-minio", metav1.DeleteOptions{})
-	//if err != nil && !k8serrors.IsNotFound(err) {
-	//	return fmt.Errorf("failed to delete minio statefulset: %v", err)
-	//}
-	//
-	//err = client.AppsV1().Deployments(config.MinioNamespace).Delete(context.TODO(), "minio", metav1.DeleteOptions{})
-	//if err != nil && !k8serrors.IsNotFound(err) {
-	//	return fmt.Errorf("failed to delete minio deployment: %v", err)
-	//}
-	//
-	//err = client.CoreV1().Namespaces().Delete(context.TODO(), config.MinioNamespace, metav1.DeleteOptions{})
-	//if err != nil && !k8serrors.IsNotFound(err) {
-	//	return fmt.Errorf("failed to delete minio namespace: %v", err)
-	//}
+	err = client.CoreV1().Namespaces().Delete(context.TODO(), config.MinioNamespace, metav1.DeleteOptions{})
+	if err != nil && !k8serrors.IsNotFound(err) {
+		return fmt.Errorf("delete minio namespace: %v", err)
+	}
 
 	return nil
-}
-
-func syncBucket(ctx context.Context, src *minio.Client, dst *minio.Client, bucket string) (int, error) {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	count := 0
-
-	exists, err := dst.BucketExists(ctx, bucket)
-	if err != nil {
-		return count, fmt.Errorf("failed to check if bucket %q exists in destination: %v", bucket, err)
-	}
-	if !exists {
-		if err := dst.MakeBucket(ctx, bucket, minio.MakeBucketOptions{}); err != nil {
-			return count, fmt.Errorf("failed to make bucket %q in destination: %v", bucket, err)
-		}
-	}
-
-	srcObjectInfoChan := src.ListObjects(ctx, bucket, minio.ListObjectsOptions{Recursive: true})
-
-	for srcObjectInfo := range srcObjectInfoChan {
-		srcObject, err := src.GetObject(ctx, bucket, srcObjectInfo.Key, minio.GetObjectOptions{})
-		if err != nil {
-			return count, fmt.Errorf("get %s from source: %v", srcObjectInfo.Key, err)
-		}
-
-		_, err = dst.PutObject(ctx, bucket, srcObjectInfo.Key, srcObject, srcObjectInfo.Size, minio.PutObjectOptions{
-			ContentType:     srcObjectInfo.ContentType,
-			ContentEncoding: srcObjectInfo.Metadata.Get("Content-Encoding"),
-		})
-		_ = srcObject.Close()
-		if err != nil {
-			return count, fmt.Errorf("failed to copy object %s to destination: %v", srcObjectInfo.Key, err)
-		} else {
-			fmt.Printf("copied object %s to destination\n", srcObjectInfo.Key)
-		}
-
-		count++
-	}
-
-	return count, nil
 }
 
 func migrateStorageClasses(config ekcoops.Config, client kubernetes.Interface) error {
@@ -375,12 +203,12 @@ func migrateStorageClasses(config ekcoops.Config, client kubernetes.Interface) e
 
 	err := migrate.Migrate(context.TODO(), fileLog, client, options)
 	if err != nil {
-		return fmt.Errorf("failed to run pvmigrate: %v", err)
+		return fmt.Errorf("run pvmigrate: %v", err)
 	}
 
 	err = client.StorageV1().StorageClasses().Delete(context.TODO(), "scaling", metav1.DeleteOptions{})
 	if err != nil {
-		return fmt.Errorf("failed to delete scaling storage class: %v", err)
+		return fmt.Errorf("delete scaling storage class: %v", err)
 	}
 	return nil
 }
